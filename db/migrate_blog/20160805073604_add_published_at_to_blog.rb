378f03f4111efae0985282f7b432f825817e13b0
dany1468 <dany1468@gmail.com>
2016-08-08 11:50:26 +0900

    Add each db migration files and schema.rb files

---
+++ db/migrate_blog/20160805073604_add_published_at_to_blog.rb
@@ -0,0 +1,5 @@
+class AddPublishedAtToBlog < ActiveRecord::Migration
+  def change
+    add_column :blogs, :published_at, :datetime
+  end
+end
---
+++ db/migrate_user/20160805073756_add_avatar_uri_to_user.rb
@@ -0,0 +1,5 @@
+class AddAvatarUriToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :avatar_uri, :string
+  end
+end
