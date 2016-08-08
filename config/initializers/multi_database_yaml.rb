f02df04829d0156ab60f6c79e7cd0be87b264f08
dany1468 <dany1468@gmail.com>
2016-08-08 11:48:44 +0900

    Add MultiDatabaseYaml and create each database.yml

---
+++ config/initializers/multi_database_yaml.rb
@@ -0,0 +1,11 @@
+module MultiDatabaseYaml
+  def self.slice(database_name, default_config_file = 'database.yml')
+    multi_db_config = YAML.load_file(Rails.root.join('config', default_config_file))
+
+    %w(production development test).each_with_object({}) {|env, configs|
+      config_key = "#{env}_#{database_name}"
+
+      configs[env] = multi_db_config[config_key] if multi_db_config.key?(config_key)
+    }.to_yaml
+  end
+end
